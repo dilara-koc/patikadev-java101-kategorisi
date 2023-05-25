44ed3d908e5369efa898986d2cfd278462762bd8
Dilara Koc <kocdilara96@gmail.com>
2023-05-25 18:18:59 +0300

    Practice18-added

---
+++ practices/yildizlarla-elmas/README.md
@@ -0,0 +1,5 @@
+# Patika.dev Java101 eğitimi Yıldızlar ile Üçgen Yapımı pratiğidir.
+
+* Çalışmanın içerik ve detayları için lütfen [linki](https://academy.patika.dev/courses/java101/pratik-yildiz-ucgen) takip ediniz.
+
+[Patika.dev](https://www.patika.dev/tr)'e teşekkürler!
---
+++ practices/yildizlarla-elmas/src/AsteriskDiamond.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class AsteriskDiamond {
+    public static void main(String[] args) {
+
+        //Making diamond shape from asterisk
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the number: ");
+        number = input.nextInt();
+
+        //First half of the diamond (it is a triangle)
+        for (int i = 1; i <= number; i++){
+
+            for (int k = 1; k <= (number - i); k++){
+                System.out.print(" ");
+            }
+            System.out.print("/");
+            for (int j = 1; j <= ((2*i)-1); j++){
+                System.out.print("*");
+            }
+            System.out.print("\\");
+            System.out.println();
+        }
+
+        //Second half of the diamond (it is a inverted triangle)
+        for (int i = 1; i <= number; i++){
+
+            for (int l = 1; l <= (i - 1); l++){
+                System.out.print(" ");
+            }
+            System.out.print("\\");
+            for (int m = 1; m <= ((2 * (number - i)) + 1); m++){
+                System.out.print("*");
+            }
+            System.out.print("/");
+            System.out.println();
+        }
+    }
+}
