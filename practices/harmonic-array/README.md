70b0a0ae3889ee107e312adff19f9294ba1cce96
Dilara Koc <kocdilara96@gmail.com>
2023-06-02 10:09:53 +0300

    Practice26-added

---
+++ practices/harmonic-array/README.md
@@ -0,0 +1,5 @@
+# Patika.dev Java101 eğitimi Harmonik Ortalama Program pratiğidir.
+
+* Çalışmanın içerik ve detayları için lütfen [linki](https://academy.patika.dev/courses/java101/pratik-array-ortalama) takip ediniz.
+
+[Patika.dev](https://www.patika.dev/tr)'e teşekkürler!
---
+++ practices/harmonic-array/src/HarmonicArray.java
@@ -0,0 +1,10 @@
+public class HarmonicArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        double harmonic = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            harmonic += 1.0 / numbers[i];
+        }
+        System.out.println(harmonic);
+    }
+}
